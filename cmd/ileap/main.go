5786979df6c27061abb98c9b15682515b12e95f6
Oscar Söderlund <odsod@users.noreply.github.com>
2025-08-18 21:23:18 +0200

    feat(cli): use charmbracelet
    Nicer CLI output.

--- cmd/ileap/main.go
+++ cmd/ileap/main.go
@@ -1,18 +1,43 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"image/color"
 	"os"
 
+	"github.com/charmbracelet/fang"
+	"github.com/charmbracelet/lipgloss/v2"
 	"github.com/spf13/cobra"
 	"github.com/way-platform/ileap-go"
 	"github.com/way-platform/ileap-go/cmd/ileap/internal/auth"
 )
 
 func main() {
-	if err := newRootCommand().Execute(); err != nil {
-		_, _ = fmt.Fprintln(os.Stderr, err)
+	if err := fang.Execute(
+		context.Background(),
+		newRootCommand(),
+		fang.WithColorSchemeFunc(func(c lipgloss.LightDarkFunc) fang.ColorScheme {
+			base := c(lipgloss.Black, lipgloss.White)
+			baseInverted := c(lipgloss.White, lipgloss.Black)
+			return fang.ColorScheme{
+				Base:         base,
+				Title:        base,
+				Description:  base,
+				Comment:      base,
+				Flag:         base,
+				FlagDefault:  base,
+				Command:      base,
+				QuotedString: base,
+				Argument:     base,
+				Help:         base,
+				Dash:         base,
+				ErrorHeader:  [2]color.Color{baseInverted, base},
+				ErrorDetails: base,
+			}
+		}),
+	); err != nil {
 		os.Exit(1)
 	}
 }
@@ -22,10 +47,30 @@ func newRootCommand() *cobra.Command {
 		Use:   "ileap",
 		Short: "iLEAP CLI",
 	}
-	cmd.AddCommand(auth.NewCommand())
+	cmd.AddGroup(&cobra.Group{
+		ID:    "pcf",
+		Title: "Product Carbon Footprints",
+	})
 	cmd.AddCommand(newGetFootprintCommand())
 	cmd.AddCommand(newListFootprintsCommand())
+	cmd.AddGroup(&cobra.Group{
+		ID:    "tad",
+		Title: "Transport Activity Data",
+	})
 	cmd.AddCommand(newListTADsCommand())
+	cmd.AddGroup(&cobra.Group{
+		ID:    "auth",
+		Title: "Authentication",
+	})
+	authCmd := auth.NewCommand()
+	authCmd.GroupID = "auth"
+	cmd.AddCommand(authCmd)
+	cmd.AddGroup(&cobra.Group{
+		ID:    "utils",
+		Title: "Utils",
+	})
+	cmd.SetHelpCommandGroupID("utils")
+	cmd.SetCompletionCommandGroupID("utils")
 	return cmd
 }
 
@@ -33,6 +78,7 @@ func newGetFootprintCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "footprint",
 		Short: "Get a product carbon footprint",
+		GroupID: "pcf",
 		Args:  cobra.ExactArgs(1),
 	}
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
@@ -56,6 +102,7 @@ func newListFootprintsCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "footprints",
 		Short: "List product carbon footprints",
+		GroupID: "pcf",
 	}
 	limit := cmd.Flags().Int("limit", 100, "max footprints queried")
 	filter := cmd.Flags().String("filter", "", "filter footprints by OData filter")
@@ -81,6 +128,7 @@ func newListTADsCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "tad",
 		Short: "List transport activity data (TAD)",
+		GroupID: "tad",
 	}
 	limit := cmd.Flags().Int("limit", 100, "max TADs queried")
 	cmd.RunE = func(cmd *cobra.Command, _ []string) error {
